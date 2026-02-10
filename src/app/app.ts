f47f2b063f080d0f246f161f7396e00180c91623
vrevanthsai <rinkurevanth2@gmail.com>
2026-02-10 18:47:26 +0530

    jira3- added routing setup and its template logic and created home page component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { Navbar } from './components/layout/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, Navbar],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected title = 'Collection_Media_Fend';
