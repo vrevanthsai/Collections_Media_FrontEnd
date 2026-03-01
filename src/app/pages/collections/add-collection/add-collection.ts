e1264571800a6c886f420304e866bb1369b9bdee
vrevanthsai <rinkurevanth2@gmail.com>
2026-03-01 19:46:44 +0530

    created add collection page and added its navigation and created new intro page component

---
+++ src/app/pages/collections/add-collection/add-collection.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-collection',
+  imports: [],
+  templateUrl: './add-collection.html',
+  styleUrl: './add-collection.scss',
+})
+export class AddCollection {
+
+}
